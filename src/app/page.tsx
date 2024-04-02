f7d719fa729eb525b6232f70fa445b0e57160012
Aurimas Cincikas <aurcin@gmail.com>
2024-04-02 21:57:35 +0300

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,39 @@
-import { Camera } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 import { Button } from '@/components/ui/button';
+import paths from '@/utils/paths';
 
 export default function Home() {
   return (
-    <div className='h-screen flex items-center justify-center'>
-      <Button>default button</Button>
-      <Button variant='outline' size='icon'>
-        <Camera />
-      </Button>
-    </div>
+    <main>
+      <header className='max-w-6xl mx-auto px-4 sm:px-8 py-6'>
+        <Image src='/logo.svg' alt='logo' width={164} height={50} priority />
+      </header>
+      <section className='max-w-6xl mx-auto px-4 sm:px-8 h-screen -mt-28 grid lg:grid-cols-[1fr,400px] items-center'>
+        <div>
+          <h1 className='capitalize text-4xl md:text-7xl font-bold'>
+            job <span className='text-primary'>tracking</span> app
+          </h1>
+          <p className='leading-loose max-w-md mt-4'>
+            Unlock Your Potential with Jobify: Your Ultimate Job Application
+            Companion. Effortlessly Manage Every Step of Your Job Search, from
+            Applications to Interviews. Stay Ahead, Stay Informed, and Achieve
+            Your Career Goals with Confidence.
+          </p>
+          <Button asChild className='mt-4'>
+            <Link href={paths.addJob()}>Get Started</Link>
+          </Button>
+        </div>
+        <Image
+          src='/main.svg'
+          alt='landing'
+          width={874}
+          height={771}
+          priority
+          className='hidden lg:block'
+        />
+      </section>
+    </main>
   );
 }
